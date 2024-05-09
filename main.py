63de39235d95fd204312d8bf214a301fc76d1d78
meligavincent <meligavincenttheophane@gmail.com>
2024-05-09 18:41:46 +0100

    feat: add reading of pdf scanned documents
    BREAKING CHANGE:

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI, File, UploadFile
 import cv2
 import pytesseract
 import numpy as np
+import docx
+import fitz
+from pdf2image import convert_from_path 
+from docx.enum.section import WD_SECTION
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -40,7 +44,6 @@ async def create_file(file: Annotated[bytes, File()]):
 
 
 def create_upload_files(files: list[UploadFile]):
-   
     for file in files:
         with open(f"files/{file.filename}", "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
@@ -53,16 +56,14 @@ async def create_upload_file(files: list[UploadFile]) -> list[FileProcessing]:
     files = create_upload_files(files=files)["filenames"]
 
     for file in files:
+        file = f'./files/{file}'
         if str(file).split(".")[-1] in ["png", "jpg", "PNG", "JPG"]:
+    
+            text = ocr(file)
 
-            
-            img = cv2.imread(f'./files/{file}')
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            threshold_img = cv2.threshold(
-                gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
-            )[1]
-            text = pytesseract.image_to_string(threshold_img)
-
+        elif str(file).split(".")[-1] in ["pdf",'PDF']:
+            read_scanned_pdf(file)
+            text = None
         else:
             text = None
         
@@ -112,3 +113,67 @@ async def get_files():
     )
 
     return data
+
+
+def valid_xml_char_ordinal(c):
+
+    codepoint = ord(c)
+    # conditions ordered by presumed frequency
+    return (
+        0x20 <= codepoint <= 0xD7FF or
+        codepoint in (0x9, 0xA, 0xD) or
+        0xE000 <= codepoint <= 0xFFFD or
+        0x10000 <= codepoint <= 0x10FFFF
+        )
+
+def ocr(file):
+    img = cv2.imread(file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    threshold_img = cv2.threshold(
+        gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )[1]
+    text = pytesseract.image_to_string(threshold_img)
+    text = ''.join(c for c in text if valid_xml_char_ordinal(c))
+    write_word_file(text)
+    return text
+
+def read_pdf(pdf_file):
+    pdf_document = fitz.open(pdf_file)
+
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document[page_num]
+        text = page.get_text()
+        
+        print(f"Page {page_num + 1}: {text}")
+
+    pdf_document.close()
+
+    return None
+
+def read_scanned_pdf(pdf_file):
+
+    pages = convert_from_path(pdf_file, 500,poppler_path='C:\\Users\\GENERAL-STORES\\Downloads\\poppler-23.08.0\\Library\\bin')
+    doc = docx.Document()
+
+    for count, page in enumerate(pages):
+        page.save(f'out{count}.jpg', 'JPEG')
+        # Add a new page break
+        
+        # Add a new paragraph on the new page
+        new_paragraph = doc.add_paragraph()
+        # Write text to the new paragraph
+        new_paragraph.text = ocr(f'out{count}.jpg')
+        doc.add_page_break()
+        os.remove(f'out{count}.jpg')
+
+    doc.save("./files/word_output/new_document.docx")
+    print("finished")
+    return doc
+
+
+def write_word_file(text):
+    doc = docx.Document()
+
+    doc.add_paragraph(text=text)
+    doc.save('demo.docx')
+    return None
